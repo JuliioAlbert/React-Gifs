62ad5a3517896a51d7f26224156f8f8cb5057757
Julio Alberto <juliioalbert98@gmail.com>
2020-07-02 14:59:55 -0500

    Primer comit

---
+++ docs/precache-manifest.c48976a7670490b41e2c86c2173591ee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd8cbeee23257609b7fbd8e926ed07dd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5fa65766acfcc777e24a",
+    "url": "/static/css/main.832776f2.chunk.css"
+  },
+  {
+    "revision": "3dfa2cb8b1df991ee1b6",
+    "url": "/static/js/2.35ce7d17.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.35ce7d17.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5fa65766acfcc777e24a",
+    "url": "/static/js/main.7a814b4b.chunk.js"
+  },
+  {
+    "revision": "8cfdda43221ff097c94c",
+    "url": "/static/js/runtime-main.c23e83e6.js"
+  }
+]);
